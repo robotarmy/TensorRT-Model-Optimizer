42fa1bf57aeaaa020915f0a36fcf05f5da7ac833
gcunhase <4861122+gcunhase@users.noreply.github.com>
2025-10-28 12:46:30 -0400

    Add generic function to detect ops not supported in low precision and exclude from conversion
    Signed-off-by: gcunhase <4861122+gcunhase@users.noreply.github.com>

--- modelopt/onnx/autocast/graphsanitizer.py
+++ modelopt/onnx/autocast/graphsanitizer.py
@@ -124,9 +124,6 @@ class GraphSanitizer:
     def convert_opset(self) -> None:
         """Convert the model to the given opset version.
 
-        Args:
-            min_opset: minimum opset version to use
-
         The method checks all opset imports and converts the model if any are below the minimum version.
         """
         # Check all opset imports
